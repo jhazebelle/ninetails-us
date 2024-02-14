5b5beb99c6588347568ee03b9099e7a1610ae6f3
Jhasmin Gambon <104863683+jhazebelle@users.noreply.github.com>
2024-02-14 21:57:50 +0000

    Created a multiplication calculator program

---
+++ ProjectPython.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter 1st number:"))
+# State the input for the first number
+num2 = int(input("Enter 2nd number:"))
+# State the input for the first number
+mul = num1 * num2
+# Add num1 and num2 and store them in sum
+print(mul)
+# The sum is printed to the screen 

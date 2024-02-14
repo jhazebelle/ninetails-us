275d9a48cad1a01cefa82e6f70a3866733ebc2d3
Jhasmin Gambon <104863683+jhazebelle@users.noreply.github.com>
2024-02-14 23:27:54 +0000

    Changed the old sum comments to product comments

--- ProjectMultiplicationCalculator.py
+++ ProjectMultiplicationCalculator.py
@@ -3,6 +3,6 @@ num1 = int(input("Enter 1st number:"))
 num2 = int(input("Enter 2nd number:"))
 # State the input for the first number
 mul = num1 * num2
-# Add num1 and num2 and store them in sum
+# Multiply num1 and num2 with the asterisk symbol and store them in sum
 print(mul)
-# The sum is printed to the screen 
+# The product is printed to the screen 

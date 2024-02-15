61e479a52e3b9b4c6647a7b04c1d0658f3dc5488
Jhasmin Gambon <104863683+jhazebelle@users.noreply.github.com>
2024-02-15 05:52:46 +0000

    Created a Mad Libs Game

---
+++ ProjectMadLibs.py
@@ -0,0 +1,197 @@
+print("Welcome to Jhasmin's Mad Libs Game!")
+
+print("\n")
+print("How To Play:")
+print("This is a game where you, the player, will fill in the blanks in a story. The story will not be revealed until the player has provided all 29 words to the story!")
+
+input("Type 'Enter' to begin:")
+
+### USER INPUTS VALUES ###
+
+#paragraph1 user inputs
+varadje1 = input("Enter an adjective:")
+varnoun2 = input("Enter a noun:")
+varadje3 = input("Enter an adjective:")
+varname4 = input("Enter a name:")
+varverb5 = input("Enter a verb ending in -ing:")
+varnoun6 = input("Enter a noun plural(more than one thing):")
+print("Congratulations for completing part 1! 4 more parts to go!")
+input("Type 'Enter' to begin:")
+
+#paragraph2 user inputs
+varverb7 = input("Enter a verb ending in -ing:")
+varnoun8 = input("Enter a noun:")
+varadje9 = input("Enter an adjective with a noun:")
+varnoun10 = input("Enter a noun:")
+varadje11 = input("Enter an adjective:")
+varadje12 = input("Enter an adjective with a noun:")
+varverb13 = input("Enter a verb:")
+print("You've completed part 2! 3 more parts to go!")
+input("Type 'Enter' to continue:")
+
+#paragraph3 user inputs
+varverb14 = input("Enter a verb ending in -ed:")
+varnoun15 = input("Enter a noun:")
+varverb16 = input("Enter a verb:")
+varnoun17 = input("Enter a noun:")
+varnoun18 = input("Enter a noun:")
+varverb19 = input("Enter a verb past tense:")
+varnoun20 = input("Enter a noun:")
+varverb21 = input("Enter a verb:")
+varverb22 = input("Enter a verb:")
+print("You're more than halfway done and completed part 3! 2 more parts to go!")
+input("Type 'Enter' to continue:")
+
+#paragraph4 user inputs
+varverb23 = input("Enter a verb:")
+varnoun24 = input("Enter a noun:")
+varverb25 = input("Enter a verb past tense:")
+varnoun26 = input("Enter a noun:")
+print("You're on a roll and completed part 4! 1 more part to go!")
+input("Type 'Enter' to continue:")
+
+#paragraph5 user inputs
+varnoun27 = input("Enter a noun:")
+varverb28 = input("Enter a verb:")
+varverb29 = input("Enter a verb:")
+print("Way to go! You've compelted all parts and it's time to reveal the story with your wisely chosen words!")
+input("Type 'Enter' to reveal the story:")
+
+
+
+
+### THE STORY REVEAL ###
+
+title = "The Coding Adventure"
+print(title)
+
+
+
+
+print("\n")
+
+#Paragraph1 Sentence1: Once upon a(n) adjective time in a(n) noun far, far away, there was a(n) adjective programmer named name. 
+#Paragraph1 Sentence2: They were known throughout the land for their incredible skill in verb ending in -ing and their knack for noun plural.
+print("Once upon a(n) ")
+print(varadje1)
+print("time in a(n) ")
+print(varnoun2)
+print("far, far away, there was a(n) ")
+print(varadje3)
+print(" programmer named ")
+print(varname4)
+print(". They were known throughout the land for their incredible skill in ")
+print(varverb5)
+print("and their knack for ")
+print(varnoun6)
+print(".")
+
+
+
+
+print("\n")
+
+#Paragraph2 Sentence1: One day, while verb ending in -ing their noun, they stumbled upon a(n) adjective noun hidden deep within the noun.
+#Paragraph2 Sentence2: It was a(n) adjective and adjective noun that promised to verb anything the programmer desired.
+print("One day, while ")
+print(varverb7)
+print(" their ")
+print(varnoun8)
+print(", they stumbled upon a(n) ")
+print(varadje9)
+print(" hidden deep within the ")
+print(varnoun10)
+print(". It was a(n) ")
+print(varadje11)
+print(" and ")
+print(varadje12)
+print(" that promised to ")
+print(varverb13)
+print(" anything the programmer desired.")
+
+
+
+
+print("\n")
+
+#Paragraph3 Sentence1: Excitedly, they verb ending in -ed the noun and began to verb with the noun. But little did they know, this noun was cursed! 
+#Paragraph3 Sentence2: Every time they verb past tense, the entire noun would verb and verb uncontrollably.
+print("Excitedly, they ")
+print(varverb14)
+print(" the ")
+print(varnoun15)
+print(" and began to ")
+print(varverb16)
+print(" with the ")
+print(varnoun17)
+print(". But little did they know, this ")
+print(varnoun18)
+print(" was cursed! Every time they ")
+print(varverb19)
+print(", the entire ")
+print(varnoun20)
+print(" would ")
+print(varverb21)
+print(" and ")
+print(varverb22)
+print(" uncontrollably.")
+
+
+
+
+print("\n")
+
+#Paragraph4 Sentence1: Despite the challenges, our brave programmer refused to verb! 
+#Paragraph4 Sentence2: With determination and noun, they tirelessly debugged and verb past tense until finally, they conquered the noun.
+print("Despite the challenges, our brave programmer refused to ")
+print(varverb23)
+print("! With determination and ")
+print(varnoun24)
+print(", they tirelessly debugged and ")
+print(varverb25)
+print(" until finally, they conquered the ")
+print(varnoun26)
+print(" .")
+
+
+
+
+print("\n")
+
+#Paragraph5 Sentence1: From that day forward, they were hailed as a hero in the world of noun. 
+#Paragraph5 Sentence2: And the lesson they learned? Always verb your code, for you never know what unexpected surprises may verb along the way.
+print("From that day forward, they were hailed as a hero in the world of ")
+print(varnoun27)
+print(". And the lesson they learned? Always ")
+print(varverb28)
+print(" your code, for you never know what unexpected surprises may ")
+print(varverb29)
+print(" along the way.")
+
+
+
+
+print("\n")
+
+
+
+conclusion = "The end"
+print(conclusion)
+
+input("Type 'Enter' to end the game:")
+
+### ENDING NOTES ###
+
+print("\n")
+
+print(" Did you like the game? Follow me on github.com/jhazebelle or linkedin.com/in/jhasmingambon")
+
+print("\n")
+
+print(" If you have any feedbacks or suggestions, please feel free to message me on both platforms!")
+
+print("\n")
+
+print("Thank you for playing Jhasmin's Mad Libs Game! *You are given a Level 1 badge shaped like a book*")
+input("Type 'T' to turn the badge around:")
+print("*The back of the badge says 'You are destined to start Jhasmin's text-based RPG game'*")

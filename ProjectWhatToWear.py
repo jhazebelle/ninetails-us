012d6f2a0b8be829f65f443ffed0c3419d19917e
Jhasmin Gambon <104863683+jhazebelle@users.noreply.github.com>
2024-02-17 03:36:52 +0000

    Developed an Outfit Application depending on the weather

---
+++ ProjectWhatToWear.py
@@ -0,0 +1,24 @@
+print("What to Wear Application")
+
+print("This application takes your input on today's weather, then interprets it to give you a recommendation on what to wear today.")
+input("Press 'Enter' to start:")
+
+
+print(f"It is ____ outside")
+choice = input("Fill in the blank:")
+
+match choice:
+     case "sunny":
+          print("Wear a hat, short sleeves, and shorts")
+     case "wet":
+          print("Wear a hoodie or raincoat and bring an umbrella")
+     case "raining":
+          print("Wear a hoodie or raincoat and bring an umbrella")
+     case "rainy":
+          print("Wear a hoodie or raincoat and bring an umbrella")
+     case "windy":
+          print("Wear a windbreaker")
+     case "snowy":
+          print("Dress in your snowjacket, thick pants, boots, gloves, and beanie")
+     case _:
+          print("Wear your usual outfit")

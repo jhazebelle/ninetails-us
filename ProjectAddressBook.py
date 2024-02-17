f272b576026f215cf90bd16d6429c48edf3e4649
Jhasmin Gambon <104863683+jhazebelle@users.noreply.github.com>
2024-02-17 19:34:21 +0000

    Renamed from a game to address book

---
+++ ProjectAddressBook.py
@@ -0,0 +1,16 @@
+def main():
+    address_book =[]
+
+    while True:
+
+        print("\n1. Add Contact")
+        print("2. Remove Contact")
+        print("3. View Contacts")
+        print("4. Clear Contacts")
+        print("5. Exit")
+
+
+        choice = input("Choose an action: ")
+
+        if choice == "1":
+            

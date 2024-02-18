8b8920ea8983d679b0ddff66bb007d441ace24ef
Jhasmin Gambon <104863683+jhazebelle@users.noreply.github.com>
2024-02-18 08:24:58 +0000

    Programmed loop, defined functions, and added input variables

--- ProjectAddressBook.py
+++ ProjectAddressBook.py
@@ -1,5 +1,32 @@
+
+# Defining Functions
+
+def add_contact(address_book, info):
+    address_book.append(info)
+    print(f"{info} added to the address book.")
+
+
+def remove_contact(address_book, info):
+    if info in address_book:
+        address_book.remove(info)
+        print(f"{info} removed from the address book.")
+
+
+def view_contacts(address_book):
+    print("Address Book:")
+    for info in address_book:
+        print('-', info)
+
+
+def clear_contacts(address_book):
+    address_book.clear()
+    print("Address book cleared.")
+
+
+
+
 def main():
-    address_book =[]
+    address_book = []
 
     while True:
 
@@ -13,4 +40,22 @@ def main():
         choice = input("Choose an action: ")
 
         if choice == "1":
-            
+            info = input("Enter the name.phonenumber to add: ")
+            add_contact(address_book, info)
+        elif choice == "2":
+            info = input("Enter the name.phonenumber to remove: ")
+            remove_contact(address_book, info)
+        elif choice == "3":
+            view_contacts(address_book)
+        elif choice == "4":
+            clear_contacts(address_book)
+        elif choice == "5":
+            print ("Exiting address book")
+            break 
+
+        else:
+            print("Invalid option. Please try choosing a number from the list of choices.")
+
+
+if __name__ == "__main__":
+    main()

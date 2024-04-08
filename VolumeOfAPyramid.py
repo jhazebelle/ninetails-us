c05d04811c633258d3eedaf1a6b2a8390be7a54d
Jhasmin Gambon <104863683+jhazebelle@users.noreply.github.com>
2024-04-07 17:39:30 -0700

    Create VolumeOfAPyramid.py

---
+++ VolumeOfAPyramid.py
@@ -0,0 +1,8 @@
+def calc_pyramid_volume(base_length, base_width, pyramid_height):
+    volume = base_length * base_width * pyramid_height * 1/3
+    return volume
+
+length = float(input())
+width = float(input())
+height = float(input())
+print('Volume for', length, width, height, "is:", calc_pyramid_volume(length, width, height))

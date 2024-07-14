e63458312e7d5da4b23325a9c28394b3f7d3e86a
Jhasmin Gambon <104863683+jhazebelle@users.noreply.github.com>
2024-07-14 14:28:30 -0700

    Create HoursAsMinutes.java

---
+++ HoursAsMinutes.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class HourToMinConv {
+   public static double getMinutesAsHours(double origMinutes) {
+
+      return origMinutes / 60;
+
+   }
+
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      double minutes;
+
+      minutes = scnr.nextDouble();
+
+      // Will be run with 210.0, 3600.0, and 0.0.
+      System.out.println(getMinutesAsHours(minutes));
+   }
+}

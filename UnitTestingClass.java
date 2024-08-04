5cd52a0dcee4fe7121dc4112643556f45c6fcf1c
Jhasmin Gambon <104863683+jhazebelle@users.noreply.github.com>
2024-08-03 19:05:47 -0700

    Create UnitTestingClass.java

---
+++ UnitTestingClass.java
@@ -0,0 +1,50 @@
+// ===== Code from file InventoryTag.java =====
+public class InventoryTag {
+   private int quantityRemaining;
+
+   public InventoryTag() {
+      quantityRemaining = 0;
+   }
+
+   public int getQuantityRemaining() {
+      return quantityRemaining;
+   }
+
+   public void addInventory(int numItems) {
+      if (numItems > 10) {
+         quantityRemaining = quantityRemaining + numItems;
+      }
+   }
+}
+// ===== end =====
+
+// ===== Code from file CallInventoryTag.java =====
+import java.util.Scanner;
+
+public class CallInventoryTag {
+   public static void main(String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      InventoryTag redSweater = new InventoryTag();
+      int sweaterShipment;
+      int sweaterInventoryBefore;
+
+      sweaterInventoryBefore = redSweater.getQuantityRemaining();
+      sweaterShipment = scnr.nextInt();
+
+      System.out.println("Beginning tests.");
+
+      // FIXME add unit test for addInventory
+      int expectedInventory;
+            
+      redSweater.addInventory(sweaterShipment);
+
+      expectedInventory = sweaterInventoryBefore + sweaterShipment;
+
+      if (redSweater.getQuantityRemaining() != expectedInventory) {
+         System.out.println("   UNIT TEST FAILED: addInventory()");
+      }
+
+      System.out.println("Tests complete.");
+   }
+}
+// ===== end =====
